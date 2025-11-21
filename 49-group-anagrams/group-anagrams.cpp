984b14a76fd2e528b319d28bf73cf9a49a0b39f4
Muhammad Ahmad <ahmadmd1845@gmail.com>
2025-11-21 12:16:28 +0500

    Time: 63 ms (14.87%) | Memory: 31 MB (17.47%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+            int freq[26] = {0};
+
+            for (char c : s) {
+                freq[c - 'a']++;
+            }
+
+            // convert freq array to a unique key string
+            string key = "";
+            for (int i = 0; i < 26; i++) {
+                key += to_string(freq[i]) + "#"; 
+            }
+
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+
+        for (auto &p : mp) {
+            result.push_back(p.second);
+        }
+
+        return result;
+    }
+};
